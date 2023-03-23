9f38a9d995dc60eda682af54f0bba0e2a38ff0f6
ShagunSG <shaguneghatak@gmail.com>
2023-03-23 21:40:45 +0530

    heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar'
+import Heading from './components/Heading'
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Heading></Heading>
     </div>
   );
 }
