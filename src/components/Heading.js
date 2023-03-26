3f454bc5e9355170cf625e33e13d22a896a3313c
ShagunSG <shaguneghatak@gmail.com>
2023-03-26 14:34:57 +0530

    .

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -5,15 +5,17 @@ export default function Heading() {
     return (
         <div>
             
-          <nav className="bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-          <div className="container flex flex-wrap items-center justify-between mx-auto" id="navbar-sticky">
-          <a href='#' className="flex items-center">
+          <nav className="bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+          <div className="container flex flex-col items-center justify-between mx-auto" id="navbar-sticky">
+          <a href='#' className="flex-col items-center">
             <img src={picture} className="h-6 mr-3 sm:h-9" alt="IIT Indore Logo"/>
-            <span className="self-center text-3xl font-semibold whitespace-nowrap">परामर्श सेवाएं | Counseling Services</span>
-            <span className='text-left'>भारतीय प्रौद्योगिकी संस्थान, इंदौर | Indian Institute of Technology, Indore</span>
+            <span>
+            <span className="self-center text-3xl font-semibold">परामर्श सेवाएं | Counseling Services</span> <br/>
+            <span className="text-left">भारतीय प्रौद्योगिकी संस्थान, इंदौर | Indian Institute of Technology, Indore</span>
+            </span>
           </a>
           </div>
           </nav>
-          </div>
+        </div>
     )
 }
