1afc6389cdf944614d4e59d9a6afeae947a8b986
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-04-04 17:42:20 +0530

    testimonials

--- src/components/testimonials/content.js
+++ src/components/testimonials/content.js
@@ -2,10 +2,19 @@ import React from "react";
 
 export default function Content(props){
     return(
-        <span>
-            //image of double quotes
-            <span>{props.testimonial}</span>
-            //ending double quotes image
-        </span>
+        <div className="w-full p-8 bg-indigo-600 rounded-md shadow-lg dark:bg-gray-800">
+            <p className="leading-loose text-white dark:text-gray-400">
+            “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ea
+            tempora dolores qui eius pariatur odit ad voluptas iste, cum
+            accusantium beatae tempore quasi nesciunt distinctio. ”
+            </p>
+            <div className="mt-6 -mx-2">
+                <div className="mx-2 text-right">
+                    <h1 className="font-semibold text-[#79DAE8] dark:text-white">
+                    {props.name}
+                    </h1>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/testimonials/testimonial.js
+++ src/components/testimonials/testimonial.js
@@ -1,9 +1,55 @@
 import React from "react";
+import Content from "./content";
 
 export default function Testimonial(){
     return(
-        <div>
-            hi
+        <section className="relative flex">
+        <div className="flex flex-col justify-center w-full min-h-screen px-4 py-2 md:mx-24">
+            <h2 className="max-w-lg font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center">
+            Testimonial
+            </h2>
+            <div className="grid w-full grid-cols-1 gap-8 2xl:grid-cols-4 lg:mt-16 md:grid-cols-2 lg:grid-cols-3">
+                <Content name="Ishaan Mittal"></Content>
+                <Content name="Anonymous"></Content>
+                <Content name="Shagun Ghatak"></Content>
+            </div>
+                {/* <div className="items-center hidden mt-12 md:flex">
+                <button
+                    title="left arrow"
+                    className="p-2 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 hover:bg-gray-100"
+                >
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 19l-7-7 7-7"
+                    />
+                    </svg>
+                </button>
+                <button
+                    title="right arrow"
+                    className="p-2 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 lg:mx-6 hover:bg-gray-100"
+                >
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+                    </svg>
+                </button>
+                </div> */}
         </div>
+        </section>
     )
 }
