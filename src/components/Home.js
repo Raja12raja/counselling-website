90807e5f24f1ba974c48a47179a3cec850714827
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-04-06 20:34:36 +0530

    correcting imports

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,13 @@ import Carousel from './Home Components/Carousel';
 import Counsellors from './Home Components/Counsellors/Counsellors';
 import Testimonial from './Home Components/testimonials/Testimonial';
 import Forms from './Home Components/Forms/Forms';
-
+import WhoAreWe from './Home Components/About';
 
 export default function Home() {
   return (
     <div className="Home">
       <Carousel></Carousel>
+      <WhoAreWe></WhoAreWe>
       <Forms></Forms>
       <Testimonial></Testimonial>
       <Counsellors></Counsellors>
