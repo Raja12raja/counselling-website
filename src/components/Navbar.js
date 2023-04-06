bf5aeaec6edda00b90bd3bee6af15ddf97db9916
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-04-06 15:21:51 +0530

    navbar active

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,44 @@
 import React from 'react';
-import {Link} from "react-router-dom"
+import {NavLink} from "react-router-dom"
 import '../App.css'
 
 export default function Navbar() {
     return (
         <div>
-            <div class="border-gray-200 px-2 sm:px-4 py-2.5 bg-transparent">
-            <div class="container flex flex-wrap items-center justify-between mx-auto">
-            <div class="flex md:order-2">
-            <button type="button" class="text-white bg-[#7286D3] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg font-medium text-sm px-5 py-2.5 text-center mr-3 md:mr-0">Get started</button>
+            <div className="border-gray-200 px-2 sm:px-4 pt-2 bg-transparent">
+            <div className="container flex flex-wrap items-center justify-between mx-auto">
+            <div className="flex md:order-2">
+            <button type="button" className="text-white bg-[#7286D3] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg font-medium text-sm px-5 py-2.5 text-center mr-3 md:mr-0">Get started</button>
             </div>
-            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-            <ul class="flex flex-col p-4 mt-4 border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+            <ul className="flex flex-col px-4 border border-gray-100 md:flex-row md:space-x-4 md:text-sm md:font-medium md:border-0 md:bg-white">
             <li>
-            <Link to="/" class="block py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Home</Link>
+                <NavLink to="/" className="block hover:text-indigo-600">
+                    <button class="py-1 px-3">
+                        Home
+                    </button>
+                </NavLink>
             </li>
             <li>
-            <Link to="/about" class="block py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">About Us</Link>
+                <NavLink to="/about" className="block hover:text-indigo-600">
+                    <button class="py-1 px-3">
+                        About Us
+                    </button>
+                </NavLink>
             </li>
             <li>
-            <Link to="/information" class="block py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Information</Link>
+                <NavLink to="/information" className="block hover:text-indigo-600">
+                    <button class="py-1 px-3">
+                        Information
+                    </button>
+                </NavLink>
             </li>
             <li>
-            <Link to="/events" class="block py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Events</Link>
+                <NavLink to="/events" className="block hover:text-indigo-600">
+                    <button class="py-1 px-3">
+                        Events
+                    </button>
+                </NavLink>
             </li>
             </ul>
             </div>
