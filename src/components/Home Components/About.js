39a771b9895c92e89656ab26a957d2cfb43a12e7
ShagunSG <shaguneghatak@gmail.com>
2023-04-07 17:52:48 +0530

    card added

--- src/components/Home Components/About.js
+++ src/components/Home Components/About.js
@@ -7,7 +7,7 @@ export default function WhoAreWe() {
         <div className="justify-between">
             <p className="justify-center text-center text-3xl font-extrabold mt-10">Who are We?</p>
             <img src={counselorImg} alt="This is us" className="mx-44 my-12 w-2/5 h-2/5 float-left"/>
-            <div className="mx-12 my-12 h-2/5 leading-loose">University life can be fun and fulfilling, providing the students various opportunities to acquire new skills 
+            <div className="mx-12 my-12 h-2/5 leading-loose font-bold" style={{fontFamily: 'Inconsolata'}}>University life can be fun and fulfilling, providing the students various opportunities to acquire new skills 
                 and knowledge, develop their character. This is an important milestone before they join the workforce and launch their career.
                 However, adjusting to a new environment away from home and managing academic and personal demands may induce high levels of stress and 
                 anxiety. Confiding in family or friends is a good coping strategy but if the issues being faced by a student persist over a long period of 
