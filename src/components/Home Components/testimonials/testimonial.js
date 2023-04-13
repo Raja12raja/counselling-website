921ad497752c3df18685f782f0dd735e5822866d
ShagunSG <shaguneghatak@gmail.com>
2023-04-13 10:18:23 +0530

    trying to work testimonials carousel

--- src/components/Home Components/testimonials/testimonial.js
+++ src/components/Home Components/testimonials/testimonial.js
@@ -1,38 +1,6 @@
 import React from "react";
 import Content from "./content.js";
 import "../../../App.css"
-
-// testimonials.map((tesimonial, index) => {
-//     return <Card data={testimonial}/>
-// })
-
-// export default function Testimonial(){
-//     return(
-//         <section className="relative flex flex-col">
-//         <div className="flex flex-col justify-center w-full px-4 py-8 md:mx-24">
-//             <h2 className="max-w-lg font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center">
-//             Testimonial
-//             </h2>
-//         </div>
-//         <div className="flex space-x-8">
-//             {
-//                 testimonials.map((testimonial, index) => {
-//                     return ( 
-//                         <div key={index}>
-//                             <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 float-left justify-center">
-//                                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{testimonial.heading}</h5>
-//                                 <p class="font-normal text-gray-700"><Content name={testimonial.name}></Content></p>
-//                             </a>
-//                         </div>
-//                     )
-//                 })
-//             }
-        
-//         </div>
-//         </section>
-//     )
-// }
-
 import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
@@ -40,16 +8,24 @@ import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
 
 const testimonialss = [
   {
-      "heading": "-----",
-      "name": "Ishaan Mittal"
+    "heading": "-----",
+    "name": "Ishaan Mittal"
   },
   {
-      "heading": "Helpful",
-      "name": "Shagun"
+    "heading": "Helpful",
+    "name": "Shagun"
   },
   {
     "heading" : "------",
     "name" : "Anonymous"
+  },
+  {
+    "heading" : "------",
+    "name" : "Mihir"
+  },
+  {
+    "heading" : "=--=--",
+    "name" : "Aditi"
   }
 ]
 
@@ -58,9 +34,11 @@ const TestimonialCarousel = ({ testimonials }) => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
   const previousCard = () => {
-    const newIndex =
-      currentCardIndex === 0 ? testimonialss.length - 1 : currentCardIndex - 1;
-    setCurrentCardIndex(newIndex);
+    for (let i = 0; i > testimonialss.indexOf({"heading":"=--=--","name":"Aditi",},); i++) {
+      const newIndex =
+        currentCardIndex === 0 ? testimonialss.length - 1 : currentCardIndex - 1;
+      setCurrentCardIndex(newIndex);
+    }
   };
 
   const nextCard = () => {
@@ -71,12 +49,12 @@ const TestimonialCarousel = ({ testimonials }) => {
 
   return (
     <section className="relative flex flex-col">
-         <div className="flex flex-col justify-center w-full px-4 py-8 md:mx-24">
-             <h2 className="max-w-lg font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center">
-             Testimonial
-             </h2>
-         </div>
-    <div className="h-96 flex justify-center items-center">
+      <div className="flex flex-col justify-center w-full px-4 py-8 md:mx-24">
+        <h2 className="max-w-lg font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center">
+          Testimonial
+        </h2>
+      </div>
+      <div className="h-96 flex justify-center items-center">
         { testimonialss.map((testimonial, index) => {
               return( 
                 <div key={index}>
@@ -89,17 +67,14 @@ const TestimonialCarousel = ({ testimonials }) => {
             }
           )
         }
-      <button className="absolute top-1/2 left-0 ml-1.5 w-2.5" onClick={previousCard}>
-        <FontAwesomeIcon icon={faChevronLeft} />
-      </button>
-      <button className="absolute top-1/2 right-0 mr-1.5 w-2.5" onClick={nextCard}>
-        <FontAwesomeIcon icon={faChevronRight} />
-      </button>
+        <button className="absolute top-1/2 left-0 ml-1.5 w-2.5" onClick={previousCard}>
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+        <button className="absolute top-1/2 right-0 mr-1.5 w-2.5" onClick={nextCard}>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
       </div>
     </section>
   );
 };
-
-
-
 export default TestimonialCarousel;
