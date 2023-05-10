495fb60da50bf803ad767b45cb4cba56fa91f9a4
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-05-10 23:14:30 +0530

    new carousel home page

--- src/components/Home Components/Carousel.js
+++ src/components/Home Components/Carousel.js
@@ -7,41 +7,123 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
 
-const imagess = [carousel1, carousel2, carousel3, carousel4];
-
-const Carousel = ({ images }) => {
-
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const previousImage = () => {
-    const newIndex =
-      currentImageIndex === 0 ? imagess.length - 1 : currentImageIndex - 1;
-    setCurrentImageIndex(newIndex);
-  };
-
-  const nextImage = () => {
-    const newIndex =
-      currentImageIndex === imagess.length - 1 ? 0 : currentImageIndex + 1;
-    setCurrentImageIndex(newIndex);
-  };
-
+import { Carousel, Typography, Button } from "@material-tailwind/react";
+ 
+export default function carousel() {
   return (
-    <div className="relative h-96">
-      <img
-        src={imagess[currentImageIndex]}
-        alt="Carousel"
-        className="object-fill h-full w-full rounded-md"
-      />
-      <button className="absolute top-1/2 left-0 ml-1.5 w-2.5" onClick={previousImage}>
-        <FontAwesomeIcon icon={faChevronLeft} />
-      </button>
-      <button className="absolute top-1/2 right-0 mr-1.5 w-2.5" onClick={nextImage}>
-        <FontAwesomeIcon icon={faChevronRight} />
-      </button>
-    </div>
+    <Carousel className="rounded-xl carousel">
+      <div className="relative height  w-full">
+        <img
+          src={carousel2}
+          alt="image 1"
+          className="object-fill h-full w-full rounded-md"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+            >
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80"
+            >
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that
+              quality of air that emanation from old trees, that so
+              wonderfully changes and renews a weary spirit.
+            </Typography>
+          </div>
+        </div>
+      </div>
+      <div className="relative height w-full">
+        <img
+          src={carousel1}
+          alt="image 2"
+          className="object-fill h-full w-full rounded-md"
+        />
+        <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
+          <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+            >
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80"
+            >
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that
+              quality of air that emanation from old trees, that so
+              wonderfully changes and renews a weary spirit.
+            </Typography>
+          </div>
+        </div>
+      </div>
+      <div className="relative height w-full">
+        <img
+          src={carousel3}
+          alt="image 3"
+          className="object-fill h-full w-full rounded-md"
+        />
+        <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
+          <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+            >
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80"
+            >
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that
+              quality of air that emanation from old trees, that so
+              wonderfully changes and renews a weary spirit.
+            </Typography>
+          </div>
+        </div>
+        </div>
+      <div className="relative height w-full">
+        <img
+          src={carousel4}
+          alt="image 3"
+          className="object-fill h-full w-full rounded-md"
+        />
+        <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
+          <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+            >
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80"
+            >
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that
+              quality of air that emanation from old trees, that so
+              wonderfully changes and renews a weary spirit.
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </Carousel>
   );
-};
-
-
-
-export default Carousel;
+}
