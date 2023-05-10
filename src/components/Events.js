88cb5bd1d60991eb807404a0fe9e5440b827a153
ShagunSG <shaguneghatak@gmail.com>
2023-05-10 19:08:26 +0530

    events page added

--- src/components/Events.js
+++ src/components/Events.js
@@ -4,8 +4,37 @@ import React from 'react';
 
 export default function Events() {
   return (
-    <div className="App">
-        hiiiii
+    <div className="mx-1.5 my-1">
+      <h2 className="max-w-lg py-10 text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center">
+        EVENTS
+      </h2>
+      <div className='lg:container mx-auto flex md:px-16 px-10 md:flex-row flex-col items-center'>
+        <p className="leading-relaxed lg:flex-grow md:w-1/2 lg:pr-12 md:pr-8 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          The Counselling center is involved in student life at the campus through different levels of interactive processes that are all over seen and 
+          directed by the counsellor herself. <br/><br/>
+          The Counselling center’s mental health activities focus on early Identification & intervention, as well as prevention of mental health 
+          problems of students. Individual counselling sessions are conducted by the counsellor for the students who seek help on their own or are 
+          referred. The referrals usually come from Institute Medical officers, and Academic office .The volunteers and Gymkhana members also share 
+          mental health concerns of their peers with the counsellor.<br/><br/>
+          For new students and parents the orientation programme itself is the time where the counselling center is introduced and its various 
+          functions highlighted by the counsellor. Counsellor herself observes and interacts with new students in the institute and hostel campus in 
+          formal and informal settings by visiting them during their labs, tutorials, free slots etc.<br/><br/>
+          The student volunteers of the center and student Gymkhana members also help freshers by extending their helping hand both for academic and 
+          non-academic concerns. The center facilitates the peer tutoring classes where students help and each other with academic difficulties in 
+          topics.
+        </p>
+      </div>
+      <div>
+        <h3 className="leading-relaxed lg:flex-grow md:w-1/2 lg:pr-12 md:pr-8 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          World Mental Health Day programme on 13th and 14th October 2022
+        </h3>
+        <p>The Event consisited of Expert Talks by Vijendra Singh,  MD (Psychiatry)- HOD, Department of Psychiatry at All India Institute of 
+          Medical Sciences (AIIMS) Bhopal, Dr Lilavati Krishnan, a retired professor of psychology from the Department of Humanities & Social 
+          Sciences, IIT Kanpur and Dr Ashutosh Singh, MBBS, DNB (Psychiatry), Consultant Psychiatrist & De-Addiction Specialist at 
+          Apollo Hospitals, Indore. The events also had competitions for the IITI community such as the Poster Making Competition and Digital 
+          Logo Making Competition.
+        </p>
+      </div>
     </div>
   );
 }
