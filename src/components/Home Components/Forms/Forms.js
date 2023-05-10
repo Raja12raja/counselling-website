339a4f699f3aae4d1ed08fca6c6d5f2b19009480
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-05-10 18:29:42 +0530

    testimonials changes.

--- src/components/Home Components/Forms/Forms.js
+++ src/components/Home Components/Forms/Forms.js
@@ -5,9 +5,9 @@ import FeedbackImage from "../../../Images/Feedback Form logo.png";
 export default function Forms(){
     return(
         <div className="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-1 lg:pt-12">
-            {/* <h2 className="max-w-lg mb-10 mt-10 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center">
+            {<h2 className="max-w-lg mb-10 mt-10 text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center">
             Useful Forms
-            </h2> */}
+            </h2>}
             <div className="grid justify-items-center mx-auto lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-2 text-center">
                 <Content 
                     title = "Feedback Form" 
--- src/components/Home Components/testimonials/content.js
+++ src/components/Home Components/testimonials/content.js
@@ -2,19 +2,22 @@ import React from "react";
 
 export default function Content(props){
     return(
-        <div className="mx-20 md:w-full md:mx-0 p-8 bg-indigo-600 shadow-lg">
-            <p className="leading-loose text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ea
-            tempora dolores qui eius pariatur odit ad voluptas iste, cum
-            accusantium beatae tempore quasi nesciunt distinctio.
-            </p>
-            <div className="mt-4 -mx-2">
-                <div className="mx-2 text-right">
-                    <h1 className="font-semibold text-[#79DAE8] ">
+        <div className="flex flex-shrink-0 items-center">
+        <img src="https://i.ibb.co/DWrGxX6/carosel-2.png" alt="sitting area" className=" opacity-0" />
+        <div className="bg-indigo-600 bg-opacity-100 absolute w-full p-6 shadow-lg">
+        <p className="leading-loose text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ea
+        tempora dolores qui eius pariatur odit ad voluptas iste, cum
+        accusantium beatae tempore quasi nesciunt distinctio.
+        </p>
+        <div className="mt-4 -mx-2">
+            <div className="mx-2 text-right">
+                <h1 className="font-semibold text-[#79DAE8] ">
                     {props.name}
-                    </h1>
-                </div>
+                </h1>
             </div>
+        </div>
+        </div>
         </div>
     )
 }
