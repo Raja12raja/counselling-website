477c792e657a75876b9bba35715e511b017e9f37
ShagunSG <shaguneghatak@gmail.com>
2023-06-01 18:52:06 +0530

    background added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,21 @@ import Counsellors from './Home Components/Counsellors/counsellors.js';
 import TestimonialCarousel from './Home Components/testimonials/testimonial.js';
 import Forms from './Home Components/Forms/Forms.js';
 import WhoAreWe from './Home Components/About.js';
+import "../App.css"
 
 export default function Home() {
   return (
-    <div className="Home">
-      <Carousel></Carousel>
-      <WhoAreWe></WhoAreWe>
-      <Forms></Forms>
-      <TestimonialCarousel></TestimonialCarousel>
-      <Counsellors></Counsellors>
-    </div>
+    <>
+      <div className="Home">
+        <Carousel></Carousel>
+        <div className='background'></div>
+        <div className='text'>
+        <WhoAreWe></WhoAreWe>
+        <Forms></Forms>
+        <TestimonialCarousel></TestimonialCarousel>
+        <Counsellors></Counsellors>
+        </div>
+      </div>
+    </>
   );
 }
